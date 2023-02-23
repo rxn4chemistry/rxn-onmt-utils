d0642cc7bb09b6a2063ccf19d5d2e18c44458061
Alain Vaucher <ava@zurich.ibm.com>
2023-02-23 13:18:48 +0100

    Update README

--- src/rxn/onmt_utils/__init__.py
+++ src/rxn/onmt_utils/__init__.py
@@ -4,3 +4,11 @@
 # ALL RIGHTS RESERVED
 
 __version__ = "0.8.3.dev2"  # managed by bump2version
+
+from rxn.onmt_utils.translate import translate
+from rxn.onmt_utils.translator import Translator
+
+__all__ = [
+    "translate",
+    "Translator",
+]
