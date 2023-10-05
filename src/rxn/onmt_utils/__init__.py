68703cdadcdf28d8300f502cfd49525f22daea30
Alain Vaucher <ava@zurich.ibm.com>
2023-10-05 10:55:42 +0200

    Bump version: 1.0.3 → 1.1.0

--- src/rxn/onmt_utils/__init__.py
+++ src/rxn/onmt_utils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.3"  # managed by bump2version
+__version__ = "1.1.0"  # managed by bump2version
 
 from rxn.onmt_utils.translate import translate
 from rxn.onmt_utils.translator import Translator
